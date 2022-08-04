6dfaaa2dcc61253dd1ec65dab63d401703c96d60
HeitorFM2 <heitorfm3@gmail.com>
2022-08-03 21:27:32 -0300

    first commit

---
+++ src/corpo.js
@@ -0,0 +1,22 @@
+import './corpo.css';
+import Conversor from './conversor';
+function Corpo() {
+  return (
+    <div className="Corpo">
+      <div className='LinhaCorpo'>
+        <Conversor moedaA="USD" moedaB="BRL"></Conversor>
+        <Conversor moedaA="BRL" moedaB="USD"></Conversor>
+      </div>
+      <div className='LinhaCorpo'>
+        <Conversor moedaA="CAD" moedaB="BRL"></Conversor>
+        <Conversor moedaA="BRL" moedaB="CAD"></Conversor>
+      </div>
+      <div className='LinhaCorpo'>
+        <Conversor moedaA="EUR" moedaB="BRL"></Conversor>
+        <Conversor moedaA="BRL" moedaB="EUR"></Conversor>
+      </div>
+    </div>
+  );
+}
+
+export default Corpo;
